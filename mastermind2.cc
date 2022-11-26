a69e4c84392cff3349a6318495668de465c455ad
JowieEs <63318726+JowieEs@users.noreply.github.com>
2022-11-26 14:22:35 +0100

    furunculo.

--- mastermind2.cc
+++ mastermind2.cc
@@ -32,7 +32,7 @@ bool codigo_correcto(Vec &comb,int cod){
             //Inv: no se repite ningun numero en el codigo e i-1<4
             int j=i+1;
             while(not incorrect and j<4){
-                //Inv: no se repite ningun numero en el codigo, i-1<4, j-1<4 y ademas j-1!=9 y j-1!=0.
+                //Inv: no se repite ningun numero en el codigo, i-1<4, j-1<4 y además en la posicion del vector j-1 no se encuentra ningún 0 o 9.
                 if(comb[i]==comb[j] or comb[j]==9 or comb[j]==0) incorrect=true;
                 else ++j;
             }
